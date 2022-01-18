3421fde9af022b77b2163db8376cb030850229ac
Anandhusanthosh <96854928+Anandhusanthosh@users.noreply.github.com>
2022-01-18 12:00:43 +0530

    Create Ret args.c

---
+++ Ret args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int getAge(int birthYear) {
+    int currentAge;
+    currentAge = 2022 - birthYear;
+    return currentAge;
+}
+
+void main() {
+    int birthYear;
+    printf("Enter your birthYear : ");
+    scanf("%d",&birthYear);
+    printf("You are %d years old in 2022", getAge(birthYear));
+}
