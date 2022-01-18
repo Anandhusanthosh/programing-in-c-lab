d3016f473ab1752d98c1db1fbd33f574739de451
Anandhusanthosh <96854928+Anandhusanthosh@users.noreply.github.com>
2022-01-18 11:42:10 +0530

    Create no_ret__args.c

---
+++ no_ret__args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void getSum(int, int);
+
+int main() {
+    int num1, num2;
+    printf("Enter the two numbers : ");
+    scanf("%d%d",&num1,&num2);
+    getSum(num1, num2);
+}
+
+void getSum(int num1, int num2) {
+    int sum; 
+    sum = num1 + num2;
+    printf("%d + %d = %d",num1, num2, sum);
+}
